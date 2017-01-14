def7e0ec61d3df067986b1a6df0a7b953f2e82b5
Vrushita <Vrushita@Vrushitas-MacBook-Air.local>
2017-01-14 10:43:23 -0600

    new commit

--- Geolocation.py
+++ Geolocation.py
@@ -1,5 +1,8 @@
 import httplib2
 import json
+import sys
+
+
 
 def getGeocodeLocation(inputString):
     google_api_key = "AIzaSyDF9zCSziOR1fMeEouclQvWC_pf2U9iLVU"
@@ -7,7 +10,15 @@ def getGeocodeLocation(inputString):
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'%(locationString,google_api_key))
     h = httplib2.Http()
     response, content = h.request(url, 'GET')
-    result = json.loads(content)
+    if sys.version_info > (3,0):
+        true_content = content.decode('utf-8')
+    else:
+        true_content = content
+    result = json.loads(true_content)
     latitude = result['results'][0]['geometry']['location']['lat']
-    longitude = ['results'][0]['geometry']['location']['lng']
+    longitude = result['results'][0]['geometry']['location']['lng']
     return (latitude,longitude)
+
+
+response = getGeocodeLocation("Chicago, IL")
+print (response)
